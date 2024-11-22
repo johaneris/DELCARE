3aed5da645e23751fe3360cef8a97e07e008616f
Garu006 <garojas@uamv.edu.ni>
2024-11-22 12:58:09 -0600

    d

---
+++ Estructuras/StrHorario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAM_INVESTIGATION.Estructuras
+{
+    public struct Horarios
+    {
+        public int IdHorario { get; set; }
+        public int IdDoctor { get; set; }
+        public string Especialidades { get; set; }
+        public DateTime HoraInicial { get; set; }
+        public DateTime HoraFinal { get; set; }
+        public string DiaSemana { get; set; }
+
+        public Horarios(int idHorario, int idDoctor, string especialidades, DateTime horaInicial, DateTime horaFinal, string diaSemana)
+        {
+            IdHorario = idHorario;
+            IdDoctor = idDoctor;
+            Especialidades = especialidades;
+            HoraInicial = horaInicial;
+            HoraFinal = horaFinal;
+            DiaSemana = diaSemana;
+        }
+    }
+}
