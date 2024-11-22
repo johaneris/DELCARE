0df4432d768077fb9f93cd5254d2cb08c3ffabb5
johaneris <jsavalos@uamv.edu.ni>
2024-11-22 13:08:53 -0600

    steven no sabe usar bash

---
+++ Estructuras/strHorarioDoctor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAM_INVESTIGATION.Estructuras
+{
+    public class strHorarioDoctor
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Especialidad { get; set; }
+        public string Correo { get; set; }
+        public string Celular { get; set; }
+        public string Estado { get; set; }
+
+    }
+
+    public class Horario
+    {
+        public int ID { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime HoraInicial { get; set; }
+        public DateTime HoraFinal { get; set; }
+        public string DiaSemana { get; set; }
+    }
+}
