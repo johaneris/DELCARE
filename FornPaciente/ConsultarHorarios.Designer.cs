59b6026369f39f754b5aa92b31b682ecc14e1e6b
Garu006 <garojas@uamv.edu.ni>
2024-11-22 22:22:19 -0600

    aplicando logica en consultarHorarios

--- FornPaciente/ConsultarHorarios.Designer.cs
+++ FornPaciente/ConsultarHorarios.Designer.cs
@@ -38,18 +38,18 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panelMain = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmb_BuscarDoctor = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvHorarios = new System.Windows.Forms.DataGridView();
             this.Doctor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.horaInicial = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.horaFinal = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.diaSemana = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cmb_BuscarDoctor = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.panelMain.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHorarios)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -128,7 +128,7 @@
             this.panelMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
             this.panelMain.Controls.Add(this.cmb_BuscarDoctor);
             this.panelMain.Controls.Add(this.label2);
-            this.panelMain.Controls.Add(this.dataGridView1);
+            this.panelMain.Controls.Add(this.dgvHorarios);
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelMain.Location = new System.Drawing.Point(247, 60);
             this.panelMain.Margin = new System.Windows.Forms.Padding(2);
@@ -136,11 +136,34 @@
             this.panelMain.Size = new System.Drawing.Size(603, 454);
             this.panelMain.TabIndex = 6;
             // 
-            // dataGridView1
+            // cmb_BuscarDoctor
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.cmb_BuscarDoctor.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.cmb_BuscarDoctor.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_BuscarDoctor.FormattingEnabled = true;
+            this.cmb_BuscarDoctor.Items.AddRange(new object[] {
+            ""});
+            this.cmb_BuscarDoctor.Location = new System.Drawing.Point(87, 364);
+            this.cmb_BuscarDoctor.Name = "cmb_BuscarDoctor";
+            this.cmb_BuscarDoctor.Size = new System.Drawing.Size(270, 28);
+            this.cmb_BuscarDoctor.TabIndex = 50;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(59, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Buscar Doctor";
+            // 
+            // dgvHorarios
+            // 
+            this.dgvHorarios.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.dgvHorarios.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvHorarios.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(126)))), ((int)(((byte)(143)))));
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -148,19 +171,19 @@
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.PowderBlue;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.ColumnHeadersHeight = 30;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvHorarios.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvHorarios.ColumnHeadersHeight = 30;
+            this.dgvHorarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvHorarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Doctor,
             this.horaInicial,
             this.horaFinal,
             this.diaSemana});
-            this.dataGridView1.EnableHeadersVisualStyles = false;
-            this.dataGridView1.GridColor = System.Drawing.Color.Gainsboro;
-            this.dataGridView1.Location = new System.Drawing.Point(47, 49);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvHorarios.EnableHeadersVisualStyles = false;
+            this.dgvHorarios.GridColor = System.Drawing.Color.Gainsboro;
+            this.dgvHorarios.Location = new System.Drawing.Point(47, 49);
+            this.dgvHorarios.Name = "dgvHorarios";
+            this.dgvHorarios.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(126)))), ((int)(((byte)(133)))));
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -168,17 +191,17 @@
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.PowderBlue;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Gainsboro;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.RowHeadersWidth = 51;
+            this.dgvHorarios.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvHorarios.RowHeadersWidth = 51;
             dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(126)))), ((int)(((byte)(133)))));
             dataGridViewCellStyle3.Font = new System.Drawing.Font("Century", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Gainsboro;
             dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.PowderBlue;
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
-            this.dataGridView1.Size = new System.Drawing.Size(507, 252);
-            this.dataGridView1.TabIndex = 5;
-            this.dataGridView1.TabStop = false;
+            this.dgvHorarios.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvHorarios.Size = new System.Drawing.Size(507, 252);
+            this.dgvHorarios.TabIndex = 5;
+            this.dgvHorarios.TabStop = false;
             // 
             // Doctor
             // 
@@ -204,29 +227,6 @@
             this.diaSemana.Name = "diaSemana";
             this.diaSemana.Width = 115;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.DimGray;
-            this.label2.Location = new System.Drawing.Point(59, 333);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(171, 28);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Buscar Doctor";
-            // 
-            // cmb_BuscarDoctor
-            // 
-            this.cmb_BuscarDoctor.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.cmb_BuscarDoctor.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmb_BuscarDoctor.FormattingEnabled = true;
-            this.cmb_BuscarDoctor.Items.AddRange(new object[] {
-            ""});
-            this.cmb_BuscarDoctor.Location = new System.Drawing.Point(87, 364);
-            this.cmb_BuscarDoctor.Name = "cmb_BuscarDoctor";
-            this.cmb_BuscarDoctor.Size = new System.Drawing.Size(270, 28);
-            this.cmb_BuscarDoctor.TabIndex = 50;
-            // 
             // ConsultarHorarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -243,7 +243,7 @@
             this.panel2.PerformLayout();
             this.panelMain.ResumeLayout(false);
             this.panelMain.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHorarios)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -258,7 +258,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panelMain;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvHorarios;
         private System.Windows.Forms.DataGridViewTextBoxColumn Doctor;
         private System.Windows.Forms.DataGridViewTextBoxColumn horaInicial;
         private System.Windows.Forms.DataGridViewTextBoxColumn horaFinal;
--- FornPaciente/ConsultarHorarios.cs
+++ FornPaciente/ConsultarHorarios.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UAM_INVESTIGATION.Helpers;
 
 namespace UAM_INVESTIGATION.FornPaciente
 {
@@ -15,6 +16,41 @@ namespace UAM_INVESTIGATION.FornPaciente
         public ConsultarHorarios()
         {
             InitializeComponent();
+        }
+        private string ObtenerNombreDoctor(int id)
+        {
+            DoctorService doctorService = new DoctorService();
+            var doctor = doctorService.LeerDoctores();
+
+            foreach (var doc in doctor)
+            {
+                if(doc.Id == id)
+                {
+                    return doc.Nombre;
+                }
+            }
+            return "";
+        }
+
+        private void CargarHorarios()
+        {
+            ControlHorario controlHorario = new ControlHorario();
+            var horarios = controlHorario.LeerHorarios();
+           
+
+            dgvHorarios.Rows.Clear();
+            foreach (var horario in horarios )
+            {
+                string Nombre = ObtenerNombreDoctor(horario.DoctorID);
+                dgvHorarios.Rows.Add(Nombre, horario.HoraInicial, horario.HoraFinal, horario.DiaSemana);
+            }
+        }
+
+        private void FiltrarPorDoctor(string Doctor)
+        {
+            ControlHorario controlHorario = new ControlHorario();
+            var horarios = controlHorario.LeerHorarios();
+            
         }
     }
 }
